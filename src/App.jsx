c5ae451f90a8feb3081854c4ebb4ddf9eb5310ea
Guilherme Martins <g.santosmartins@outlook.com>
2021-10-01 16:45:58 -0300

    Terminando a configuração de sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import './styles/global.scss'
+
 export function App () {
-  return <h1>Hello World</h1>
+
+
+  // throw new Error("Eita Giovana, o forninho caiu!")
+  return <h1>Hello React</h1>
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'producion'
+
 
 module.exports = {
+
+  mode: isDevelopment ? 'development' : 'production',
+
+  devtool: isDevelopment ? 'eval-source-map': 'source-map',
 
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
@@ -12,6 +20,21 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
 
+  //nova forma de chamar o web-server 
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 5500,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname,  'public', 'index.html')
+    })
+  ],
+
   module: {
     rules: [
       {
@@ -19,6 +42,11 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader',
 
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       }
     ],
   }
