491819fcbb10d73f431b977db81d81289851cf79
g-santosmartins <g.santosmartins@outlook.com>
2021-10-02 12:43:08 -0300

    Terminando a listagem de repositorios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import { RepositoryItem } from "./RepositoryItem"
 import '../styles/repositories.scss'
 
@@ -10,26 +11,24 @@ const repository = {
 }
 
 export function RepositoryList() {
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/g-santosmartins/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+
+  }, [])
+
+  const [repositories, setRepositories] = useState([]);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem
-          repository={repository}
-        />
-
-        <RepositoryItem
-          repository={repository}
-        />
-
-        <RepositoryItem
-          repository={repository}
-        />
-
-        <RepositoryItem
-          repository={repository}
-        />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository}/> 
+        })}
       </ul>
 
     </section>
